d87e9a2d1349ac44bc0a0ce70e75e99414051297
9hafidz6 <9hafidz6@gmail.com>
2019-08-22 21:45:32 +0800

    Added greet,echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String h_line = "_____________________________________\n\n";
+        String greet_msg = "Hello! i'm Duke\n"
+                        + "What can i do for you?\n";
+        String bye_msg = "Bye. Hope to see you again soon!";
+
+        String msg = "";
+
+        System.out.println(h_line + greet_msg + h_line);
+
+        while(true) {
+            msg = sc.nextLine();
+            if(msg.equals("bye")) {
+                System.out.println(h_line + "\t" + bye_msg + "\n"+ h_line);
+                break;
+            }
+            else {
+                System.out.println(h_line + "\t" + msg + "\n" + h_line);
+
+            }
+        }
     }
 }
+
+
