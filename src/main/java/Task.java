bd3444f20730e9e2a3208786c71a0333968dd68b
9hafidz6 <9hafidz6@gmail.com>
2019-08-25 16:19:21 +0800

    added task class to store status and description

---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+    public void markDone(){
+        isDone = true;
+    }
+    public String getStatusIcon(){
+        return (isDone ? "✔" : "✘"); //return tick or X symbols
+    }
+}
