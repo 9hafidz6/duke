aa580a23384461772c5b038924f5b91a33beac2f
9hafidz6 <9hafidz6@gmail.com>
2019-08-25 16:39:04 +0800

    level-3 mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,41 +11,48 @@ public class Duke {
         String bye_msg = "Bye. Hope to see you again soon!";
         String msg;
 
-        ArrayList<String> task_list = new ArrayList<String> (100);
+        ArrayList<Task> task_list = new ArrayList<Task> (100);
 
         System.out.println(h_line + greet_msg + h_line);
 
         while(true) {
             msg = sc.nextLine();
-            if(msg.equals("bye")) { //if user command "bye", end the program
+            if (msg.equals("bye")) {
                 System.out.println(h_line + "\t" + bye_msg + "\n"+ h_line);
                 break;
             }
-            else if(msg.equals("list")) { //if user enters the command "list", output all entries in the list
-                int a = 0;
+            else if (msg.equals("list")) { //if user enters the command "list", output all entries in the list
                 System.out.println(h_line);
-                Iterator<String> itr = task_list.iterator();
-                while(itr.hasNext()) { //outputs all entry of list from 1 ~ 100 if have
-                    a++;
-                    System.out.println(a + ". " + itr.next() + "\n");
+                for(int a = 0; a < task_list.size(); a++) { //outputs all entry of list from 1 ~ 100 if have
+                    System.out.println((a+1) + ".[" + task_list.get(a).getStatusIcon() + "] "+ task_list.get(a).description + "\n");
                 }
                 System.out.println(h_line);
             }
+            else if (msg.indexOf("done") == 0) {
+                String[] token = msg.split(" ");
+
+                task_list.get(Integer.parseInt(token[1]) - 1).markDone();
+
+                System.out.println(h_line + "\tNice! I've marked this task as done:\n"
+                        + "\t[" + task_list.get(Integer.parseInt(token[1]) - 1).getStatusIcon()
+                        + "] " + task_list.get(Integer.parseInt(token[1]) - 1).description + "\n"
+                        + h_line);
+            }
             else { //if user enters a command, add into task_list
                 boolean flag = false; //flag to check whether task exists in list
-                Iterator<String> itr1 = task_list.iterator();
-                while(itr1.hasNext()) {
-                    if(itr1.next().equals(msg)) { //if item has already been added, don't add
+                for(int a = 0; a < task_list.size(); a++) {
+                    if (task_list.get(a).description.equals(msg)) { //if item has already been added, don't add
                         flag = true;
                         break;
                     }
                 }
-                if(flag) { //if task already exist, output msg has already been added
-                    System.out.println(h_line + "\t" + msg + " has already been added" + "\n"+ h_line);
+                if (flag) { //if task already exist, output msg has already been added
+                    System.out.println(h_line + "\t" + msg + " has already been added" + "\n" + h_line);
                 }
                 else { //if task does not exist in list, add to list
-                    task_list.add(msg);
-                    System.out.println(h_line + "\t" + "added: " + msg + "\n"+ h_line); //output the added "msg"
+                    System.out.println(h_line + "\t" + "added: " + msg + "\n" + h_line); //output the added "msg"
+                    Task t = new Task(msg);
+                    task_list.add(t);
                 }
             }
         }
