9eac7554316a0dddf4d50bb060b136901c1a2a21
9hafidz6 <9hafidz6@gmail.com>
2019-08-26 21:31:48 +0800

    added Deadline, event and todo classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,15 @@
+public class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        //super.description = super.description + "(by: " + by + ")";
+        return "[D]" + "[" + super.getStatusIcon() + "] " + super.description + "(by: " + by + ")";
+    }
+}
