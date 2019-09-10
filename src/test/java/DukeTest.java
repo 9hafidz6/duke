de23e18884bc563bf94269eab50b12c329b7f500
9hafidz6 <9hafidz6@gmail.com>
2019-09-10 21:39:27 +0800

    added A-JUnit

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DukeTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2,2);
+    }
+    @Test
+    public void TestIcon() {
+        assertEquals("✘", new Deadline("do CS2113", "1/1/2019").getStatusIcon());
+    }
+    @Test
+    public void TestTodo() {
+        assertEquals("[T][✘] assignments CS2113", new ToDos("assignments CS2113").toString() );
+    }
+    @Test
+    public void TestDeadline() {
+        assertEquals("[D][✘] CS2113 project (by:Mon Sep 09 12:00:00 SGT 2019)",
+                new Deadline("CS2113 project", "Mon Sep 09 12:00:00 SGT 2019").toString());
+    }
+}
